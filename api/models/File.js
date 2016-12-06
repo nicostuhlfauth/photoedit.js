91350edadbe609d0f534070555aea78f25455200
Nicolas Stuhlfauth <noreply@users.github.com>
2016-12-06 14:25:36 +0100

    Test

--- api/models/File.js
+++ api/models/File.js
@@ -1,7 +1,7 @@
 /**
  * File.js
  *
- * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @description :: TODO: Bind mongodb database
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
 
