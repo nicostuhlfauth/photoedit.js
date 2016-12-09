10aaad9fb798736205c8d7129e27be8fc522c533
Nicolas Stuhlfauth <nicolas.stuhlfauth@stud.hs-ruhrwest.de>
2016-12-09 16:49:22 +0100

    grayscale works

--- assets/js/photo.js
+++ assets/js/photo.js
@@ -5,10 +5,10 @@
 window.onload = function() {
 
   var canvas = document.getElementById("myCanvas");
+  var ctx = canvas.getContext("2d");
 
   function loadImage(img) {
     console.log(canvas);
-    var ctx = canvas.getContext("2d");
     console.log(ctx);
     ctx.drawImage(img, 0, 0);
   }
@@ -21,6 +21,23 @@ window.onload = function() {
     canvas.width = img.naturalWidth;
     canvas.height = img.naturalHeight;
     loadImage(this);
+
+
+    // grayscale
+
+    var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    var data = imageData.data;
+
+    for (i = 0; i < data.length; i = i+4) {
+      var grayscale = data[i] * 0.3 + data[i+1] * 0.59 + data[i+2] * 0.11;
+      data[i] = grayscale;
+      data[i+1] = grayscale;
+      data[i+2] = grayscale;
+    }
+
+    ctx.putImageData(imageData, 0, 0);
+
+
   }
 
 
