544a9df4fd348376102e0ae51526fbddcf570230
Nicolas Stuhlfauth <nicolas.stuhlfauth@stud.hs-ruhrwest.de>
2016-12-09 14:41:17 +0100

    changed file structure

--- assets/js/photo.js
+++ assets/js/photo.js
@@ -1,3 +1,28 @@
 /**
  * Created by nico on 09.12.16.
  */
+
+window.onload = function() {
+
+  var canvas = document.getElementById("myCanvas");
+
+  function loadImage(img) {
+    console.log(canvas);
+    var ctx = canvas.getContext("2d");
+    console.log(ctx);
+    ctx.drawImage(img, 0, 0);
+  }
+
+  var img = new Image();
+  img.src = "http://localhost:1337/images/1e827def-21d0-428c-9d79-1892882691e5.png";
+  //var img = document.getElementById("source");
+
+  img.onload = function () {
+    canvas.width = img.naturalWidth;
+    canvas.height = img.naturalHeight;
+    loadImage(this);
+  }
+
+
+
+}
