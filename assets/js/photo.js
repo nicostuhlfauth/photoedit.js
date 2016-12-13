8c01912ae05858d33fa757f89305fbe663c57512
benediktrauch <benediktrauch@users.noreply.github.com>
2016-12-13 15:43:52 +0100

    brightness correction

--- assets/js/photo.js
+++ assets/js/photo.js
@@ -29,11 +29,15 @@ window.onload = function () {
     //ctx.putImageData(imageData, 0, 0);
 
     //monochrome (data, Schwellwert, r1, g1, b1, r2, g2,b2)
-    imageData.data.set(monochrome(data, 128, 255, 0, 0, 0, 255, 0));
-    ctx.putImageData(imageData, 0, 0);
+    //imageData.data.set(monochrome(data, 128, 255, 0, 0, 0, 255, 0));
+    //ctx.putImageData(imageData, 0, 0);
 
     //change color
-    imageData.data.set(correctColor(data, 70, 0, 0));
+    //imageData.data.set(correctColor(data, 70, 0, 0));
+    //ctx.putImageData(imageData, 0, 0);
+
+    //brightness (data, brightness - value 0 = black, 1 0 normal, >1 brighter)
+    imageData.data.set(brightness(data, 1));
     ctx.putImageData(imageData, 0, 0);
   }
 }
