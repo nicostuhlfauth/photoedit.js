134a9d5e3a7abf131ac403635e64d05ef68865f2
benediktrauch <benediktrauch@users.noreply.github.com>
2016-12-13 14:05:53 +0100

    added negative function

--- assets/js/negativeImg.js
+++ assets/js/negativeImg.js
@@ -1,13 +1,15 @@
 /**
- * Created by benedikt on 13.12.16.
+ * Created by benedikt on 11.12.16.
  */
-/*
-for (i = 0; i < data.length; i = i + 4) {
-  var negative = (255 - data[i]) + (255 - data[i + 1]) + (255 - data[i + 2]);
-  data[i] = negative;
-  data[i + 1] = negative;
-  data[i + 2] = negative;
-}
 
-ctx.putImageData(imageData, 0, 0);
-*/
+function negativeImg(data) {
+
+  for (i = 0; i < data.length; i = i + 4) {
+
+    data[i] = 255 - data[i];
+    data[i + 1] =  255 - data[i + 1];
+    data[i + 2] =  255 - data[i + 2] ;
+  }
+
+  return data;
+}
