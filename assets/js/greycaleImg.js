fd202f66ac005a336b60c27ce11fb1b7eec99979
Nicolas Stuhlfauth <nicostuhlfauth@users.noreply.github.com>
2016-12-13 13:12:48 +0100

    Grayscale works

--- assets/js/greycaleImg.js
+++ assets/js/greycaleImg.js
@@ -1,17 +1,16 @@
 /**
  * Created by benedikt on 13.12.16.
  */
-/*
-var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-var data = imageData.data;
 
 
-for (i = 0; i < data.length; i = i+4) {
-  var grayscale = data[i] * 0.3 + data[i+1] * 0.59 + data[i+2] * 0.11;
-  data[i] = grayscale;
-  data[i+1] = grayscale;
-  data[i+2] = grayscale;
+function grayscale(data) {
+
+  for (i = 0; i < data.length; i = i + 4) {
+    var grayscale = data[i] * 0.3 + data[i + 1] * 0.59 + data[i + 2] * 0.11;
+    data[i] = grayscale;
+    data[i + 1] = grayscale;
+    data[i + 2] = grayscale;
+  }
+
+  return data;
 }
-
-ctx.putImageData(imageData, 0, 0);
-*/
