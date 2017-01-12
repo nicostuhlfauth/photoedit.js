b4dfeae99a12f1077ebb15a6605dad65513ba71a
Benedikt Rauch <benediktrauch@users.noreply.github.com>
2017-01-12 17:12:40 +0100

    added all current filter to UI

--- assets/js/photo.js
+++ assets/js/photo.js
@@ -24,6 +24,8 @@ window.onload = function () {
     imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
     data = imageData.data;
 
+    //greyscale
+
     document.getElementById("btn-grayscale").addEventListener("click", function(event) {
       dataBackup.set("grayscale", data.slice());
       imageData.data.set(grayscale(data));
@@ -35,46 +37,137 @@ window.onload = function () {
       ctx.putImageData(imageData, 0, 0);
     });
 
-    //grayscale
-    //imageData.data.set(grayscale(data));
-    //ctx.putImageData(imageData, 0, 0);
-
     //negative
-    //imageData.data.set(negativeImg(data));
-    //ctx.putImageData(imageData, 0, 0);
+
+    document.getElementById("btn-negative").addEventListener("click", function(event) {
+      dataBackup.set("negative", data.slice());
+      imageData.data.set(negativeImg(data));
+      ctx.putImageData(imageData, 0, 0);
+    });
+
+    document.getElementById("negative-backup").addEventListener("click", function(event) {
+      imageData.data.set(dataBackup.get("negative"));
+      ctx.putImageData(imageData, 0, 0);
+    });
 
     //monochrome (data, Schwellwert, r1, g1, b1, r2, g2,b2)
     //imageData.data.set(monochrome(data, 64, 0, 0, 255, 0, 255, 0));
     //ctx.putImageData(imageData, 0, 0);
 
+    document.getElementById("btn-monochrome").addEventListener("click", function(event) {
+      dataBackup.set("monochrome", data.slice());
+      imageData.data.set(monochrome(data, 64, 0, 0, 255, 0, 255, 0));
+      ctx.putImageData(imageData, 0, 0);
+    });
+
+    document.getElementById("monochrome-backup").addEventListener("click", function(event) {
+      imageData.data.set(dataBackup.get("monochrome"));
+      ctx.putImageData(imageData, 0, 0);
+    });
+
     //change color
     //imageData.data.set(correctColor(data, 70, 0, 0));
     //ctx.putImageData(imageData, 0, 0);
+
+    document.getElementById("btn-rgbchanel").addEventListener("click", function(event) {
+      dataBackup.set("rgbchanel", data.slice());
+      imageData.data.set(correctColor(data, 70, 0, 0));
+      ctx.putImageData(imageData, 0, 0);
+    });
+
+    document.getElementById("rgb-backup").addEventListener("click", function(event) {
+      imageData.data.set(dataBackup.get("rgbchanel"));
+      ctx.putImageData(imageData, 0, 0);
+    });
+
 
     //brightness (data, brightness - value 0 = black, 1 0 normal, >1 brighter)
     //imageData.data.set(brightness(data, 1.5));
     //ctx.putImageData(imageData, 0, 0);
 
+    document.getElementById("btn-brightness").addEventListener("click", function(event) {
+      dataBackup.set("brightness", data.slice());
+      imageData.data.set(brightness(data, 1.5));
+      ctx.putImageData(imageData, 0, 0);
+    });
+
+    document.getElementById("brightness-backup").addEventListener("click", function(event) {
+      imageData.data.set(dataBackup.get("brightness"));
+      ctx.putImageData(imageData, 0, 0);
+    });
 
     // contrast, werte zwischen -255 und +255
     //imageData.data.set(changeContrast(data, 70));
     //ctx.putImageData(imageData, 0, 0);
 
+    document.getElementById("btn-contrast").addEventListener("click", function(event) {
+      dataBackup.set("contrast", data.slice());
+      imageData.data.set(changeContrast(data, 70));
+      ctx.putImageData(imageData, 0, 0);
+    });
+
+    document.getElementById("contrast-backup").addEventListener("click", function(event) {
+      imageData.data.set(dataBackup.get("brightness"));
+      ctx.putImageData(imageData, 0, 0);
+    });
+
     //noise reduction Schwellwert zwischen 0 und 255, je kleiner desto stärker weichzeichner
     //imageData.data.set(noiseReduction(data, img.naturalWidth, img.naturalHeight, 25));
     //ctx.putImageData(imageData, 0, 0);
+
+    document.getElementById("btn-noise").addEventListener("click", function(event) {
+      dataBackup.set("noise", data.slice());
+      imageData.data.set(noiseReduction(data, img.naturalWidth, img.naturalHeight, 25));
+      ctx.putImageData(imageData, 0, 0);
+    });
+
+    document.getElementById("noise-backup").addEventListener("click", function(event) {
+      imageData.data.set(dataBackup.get("noise"));
+      ctx.putImageData(imageData, 0, 0);
+    });
 
     //Gauß 3x3 Filter
     //imageData.data.set(gaussFilter3(data, img.naturalWidth, img.naturalHeight));
     //ctx.putImageData(imageData, 0, 0);
 
+    document.getElementById("btn-gauss3").addEventListener("click", function(event) {
+      dataBackup.set("gauss3", data.slice());
+      imageData.data.set(gaussFilter3(data, img.naturalWidth, img.naturalHeight));
+      ctx.putImageData(imageData, 0, 0);
+    });
+
+    document.getElementById("gauss3-backup").addEventListener("click", function(event) {
+      imageData.data.set(dataBackup.get("gauss3"));
+      ctx.putImageData(imageData, 0, 0);
+    });
     //Gauß 5x5 Filter
     //imageData.data.set(gaussFilter5(data, img.naturalWidth, img.naturalHeight));
     //ctx.putImageData(imageData, 0 , 0);
+
+      document.getElementById("btn-gauss5").addEventListener("click", function(event) {
+        dataBackup.set("gauss3", data.slice());
+        imageData.data.set(gaussFilter5(data, img.naturalWidth, img.naturalHeight));
+        ctx.putImageData(imageData, 0, 0);
+      });
+
+      document.getElementById("gauss5-backup").addEventListener("click", function(event) {
+        imageData.data.set(dataBackup.get("gauss5"));
+        ctx.putImageData(imageData, 0, 0);
+      });
 
     //simple Blur Filter
     //imageData.data.set(simpleBlur(data, img.naturalWidth, img.naturalHeight));
     //ctx.putImageData(imageData, 0, 0);
 
+    document.getElementById("btn-blur").addEventListener("click", function(event) {
+      dataBackup.set("blur", data.slice());
+      imageData.data.set(simpleBlur(data, img.naturalWidth, img.naturalHeight));
+      ctx.putImageData(imageData, 0, 0);
+    });
+
+    document.getElementById("blur-backup").addEventListener("click", function(event) {
+      imageData.data.set(dataBackup.get("blur"));
+      ctx.putImageData(imageData, 0, 0);
+    });
   }
 }
