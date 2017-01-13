140bd83790e15742407c09505d04752a9ce62007
Benedikt Rauch <benediktrauch@users.noreply.github.com>
2017-01-13 14:03:30 +0100

    Merge branch 'master' of C:\Users\Benedikt\Documents\GitHub\photoedit with conflicts.

--- assets/js/rotateImg.js
+++ assets/js/rotateImg.js
@@ -1,33 +1,61 @@
 /**
  * Created by benedikt on 13.12.16.
  */
-/*
-function startRotate(imageData, newArray) {
+
+function startRotate(data, width, height) {
   console.log("Hallo");
 
-  imageData.data.set(newArray);
-  ctx.putImageData(imageData, 0, 0);
+  var newArray = [];
+
+  for (var i = 0; i < height; i++) {
+    for (var j = 0; j < width; j++) {
+      //var newX = i*Math.cos(90)+ 4*j*Math.sin(90);
+      //var newY = (-1)*4*i*Math.sin(90) + 4*j*Math.cos(90);
+      newArray[newX * width + newY] = data[i * width + j];
+    }
+  }
+
+  return newArray;
 
 }
 
+function rotate90v1(data, height, width) {
+  //data = splitArray(data, 4);
+  var newArray = [];
+  var h_t = width;
+  var w_t = height;
+  console.log(data);
+
+  for (var y_t = 0; y_t <= h_t; y_t++) {
+    for (var x_t = 0; x_t <= w_t; x_t++) {
+
+      newArray[w_t * y_t + x_t] = data[(width * x_t + (height - y_t - 1))];
+      newArray[(w_t * y_t + x_t) + 1] = data[(width * x_t + (height - y_t - 1)) + 1];
+      newArray[(w_t * y_t + x_t) + 2] = data[(width * x_t + (height - y_t - 1)) + 2];
+      newArray[(w_t * y_t + x_t) + 3] = data[(width * x_t + (height - y_t - 1)) + 3];
+
+    }
+  }
+  return newArray;
+}
+
+function rotate90(data, height, width) {
+  var p_h = height;
+  var p_w = width;
+
+  data = splitArray(data, 4);
 
 
-var newArray = data;
-var imageData2 = ctx.createImageData(imageData.height, imageData.width);
+  var newArray = new Array();
 
+  for (j = 1; j <= height; j++) {
+    for (i = 0; i < width; i++) {
 
-for (var i = 0; i < img.naturalHeight; i++) {
-  for (var j = 0; j < img.naturalWidth; j++) {
-    var newX = i*Math.cos(90)+ 4*j*Math.sin(90);
-    var newY = (-1)*4*i*Math.sin(90) + 4*j*Math.cos(90);
-    newArray[newX*img.naturalWidth+newY] = data[i*img.naturalWidth+j];
-    if ((newX*img.naturalWidth+newY) == 4*i*j) startRotate(imageData2, newArray);
+    }
   }
 }
 
-imageData2.data.set(newArray);
 
-ctx.putImageData(imageData2, 0, 0);
 
-}
-*/
+
+
