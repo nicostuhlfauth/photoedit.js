510f638458618a247ec2cbf45c73749c358d43bc
Nicolas Stuhlfauth <nicostuhlfauth@users.noreply.github.com>
2017-01-14 13:09:03 +0100

    Rotating works in both directions

--- assets/js/rotateImg.js
+++ assets/js/rotateImg.js
@@ -2,53 +2,8 @@
  * Created by benedikt on 13.12.16.
  */
 
-function startRotate(data, width, height) {
-  console.log("Hallo");
 
-  var newArray = [];
-
-  for (var i = 0; i < height; i++) {
-    for (var j = 0; j < width; j++) {
-      //var newX = i*Math.cos(90)+ 4*j*Math.sin(90);
-      //var newY = (-1)*4*i*Math.sin(90) + 4*j*Math.cos(90);
-      newArray[newX * width + newY] = data[i * width + j];
-    }
-  }
-
-  return newArray;
-
-}
-
-function rotate90v1(data, height, width) {
-  //data = splitArray(data, 4);
-  var newArray = [];
-  var h_t = width;
-  var w_t = height;
-  console.log(data);
-
-  for (var y_t = 0; y_t <= h_t; y_t++) {
-    for (var x_t = 0; x_t <= w_t; x_t++) {
-
-      newArray[w_t * y_t + x_t] = data[(width * x_t + (height - y_t - 1))];
-      newArray[(w_t * y_t + x_t) + 1] = data[(width * x_t + (height - y_t - 1)) + 1];
-      newArray[(w_t * y_t + x_t) + 2] = data[(width * x_t + (height - y_t - 1)) + 2];
-      newArray[(w_t * y_t + x_t) + 3] = data[(width * x_t + (height - y_t - 1)) + 3];
-
-    }
-  }
-  return newArray;
-}
-
-function rotate90(data, height, width) {
-
-  data = splitArray(data, 4);
-
-  var newArray = new Array();
-
-}
-
-
-function rotate(data, height, width) {
+function rotateRight(data, height, width) {
   data = splitArray(data, 4);
   var newArray = [];
   for (x = 0; x < width; x++) {
@@ -59,5 +14,15 @@ function rotate(data, height, width) {
   return newArray;
 }
 
+function rotateLeft(data, height, width) {
+  data = splitArray(data, 4);
+  var newArray = [];
 
+  for (x = width; x > 0; x--) {
+    for (y = 0; y < height; y++) {
+      newArray.push(data[(width*y)+x-1][0], data[(width*y)+x-1][1], data[(width*y)+x-1][2], data[(width*y)+x-1][3]);
+    }
+  }
+  return newArray;
+}
 
